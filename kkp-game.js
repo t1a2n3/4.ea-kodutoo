1e76534f4c82d98e4ebe9ced50a45b271ab0e808
Tanel Kuklane <31847396+t1a2n3@users.noreply.github.com>
2018-05-23 14:34:43 +0300

    added files
    edited js file and added serviceworker file

--- kkp-game.js
+++ kkp-game.js
@@ -1,54 +1,67 @@
 // Kivi-käärid-paber mäng 
 // Viide algelisele mängule - aadressil http://jsfiddle.net/2jqz51Ly/ 
 // Sellelt lähtuvast algelist mängurakenduse koodi on muudetud
+//(function () {
+    //'use strict'
 
-//muutujad
-var user;
-var choose = function(choice) {
-    user = choice;
-}
+    //muutujad
+    var user;
+    var choose = function(choice) {
+        console.log('rakendus algab')
 
-//mängu loogika reeglid
-var computerChoice = Math.random();
-if (computerChoice < 0.34) {
-    computerChoice = "rock";
-}
-else if (computerChoice < 0.67) {
-    computerChoice = "paper";
-}   
-else {
-    computerChoice = "scissors";
-}
+        user = choice;
 
-var compare = function(choice1, choice2) {
-    if (choice1 === choice2) {
-        document.getElementById("result").innerHTML = "Viik!";
+        // this.registerServiceWorker()
     }
-    else if (choice1 === "rock") {
-        if (choice2 === "scissors") {
-            document.getElementById("result").innerHTML = "Sinu võit!";
+
+    //mängu loogika reeglid
+    var computerChoice = Math.random();
+        if (computerChoice < 0.34) {
+            computerChoice = "rock";
+        }
+        else if (computerChoice < 0.67) {
+            computerChoice = "paper";
+        }   
+        else {
+            computerChoice = "scissors";
+        }
+
+    var compare = function(choice1, choice2) {
+        if (choice1 === choice2) {
+            document.getElementById("result").innerHTML = "Viik!";
+        }
+        else if (choice1 === "rock") {
+            if (choice2 === "scissors") {
+                document.getElementById("result").innerHTML = "Sinu võit!";
+            }
+            else {
+                document.getElementById("result").innerHTML = "Kaotasid mängu!";
+            }
+        }
+        else if (choice1 === "paper") {
+            if (choice2 === "rock") {
+                document.getElementById("result").innerHTML = "Sinu võit!";
+            }
+            else if (choice2 ==="scissors") {
+                document.getElementById("result").innerHTML = "Kaotasid mängu!";
+            }
+        }
+        else if (choice1 === "scissors") {
+            if (choice2 === "rock") {
+                document.getElementById("result").innerHTML = "Kaotasid mängu!";
+            }
+            else if (choice2 === "paper") {
+                document.getElementById("result").innerHTML = "Sinu võit!";
+            }
         }
         else {
-            document.getElementById("result").innerHTML = "Kaotasid mängu!";
+            document.getElementById("result").innerHTML = "Peaksid nagu ka valima nende kolme vahel...";
         }
     }
-    else if (choice1 === "paper") {
-        if (choice2 === "rock") {
-            document.getElementById("result").innerHTML = "Sinu võit!";
-        }
-        else if (choice2 ==="scissors") {
-            document.getElementById("result").innerHTML = "Kaotasid mängu!";
-        }
-    }
-    else if (choice1 === "scissors") {
-        if (choice2 === "rock") {
-            document.getElementById("result").innerHTML = "Kaotasid mängu!";
-        }
-        else if (choice2 === "paper") {
-            document.getElementById("result").innerHTML = "Sinu võit!";
-        }
-    }
-    else {
-        document.getElementById("result").innerHTML = "Peaksid nagu ka valima nende kolme vahel...";
-    }
-}
+//})()
+
+
+
+
+
+
---
+++ serviceWorker.js
@@ -0,0 +1,66 @@
+// service worker
+const CACHE_NAME = 'kkp-v1'
+const urlsToCache = [
+  '/~rkaldmaa/eesrakendused/kkp/package.json',
+  '/~rkaldmaa/eesrakendused/kkp/kkp-game.js',
+  '/~rkaldmaa/eesrakendused/kkp/kkp-game.html',
+  '/~rkaldmaa/eesrakendused/kkp/',
+  '/~rkaldmaa/eesrakendused/kkp/kkp-game.css',
+  '~rkaldmaa/eesrakendused/kkp/package-lock.json'
+]
+
+self.addEventListener('install', function (event) {
+  // Perform install steps
+
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(function (cache) {
+        console.log('Opened cache')
+
+        return cache.addAll(urlsToCache)
+      })
+  )
+})
+
+self.addEventListener('fetch', function (event) {
+  // console.log('WORKER: fetch event in progress.');
+
+  if (event.request.method !== 'GET') { return }
+  event.respondWith(
+    caches
+      .match(event.request)
+      .then(function (cached) {
+        const networked = fetch(event.request)
+          .then(fetchedFromNetwork, unableToResolve)
+          .catch(unableToResolve)
+        console.log('WORKER: fetch event', cached ? '(cached)' : '(network)', event.request.url)
+        return cached || networked
+
+        function fetchedFromNetwork (response) {
+          const cacheCopy = response.clone()
+          console.log('WORKER: fetch response from network.', event.request.url)
+          caches
+            .open(version + 'pages')
+            .then(function add (cache) {
+              cache.put(event.request, cacheCopy)
+            })
+            .then(function () {
+              console.log('WORKER: fetch response stored in cache.', event.request.url)
+            })
+
+          return response
+        }
+
+        function unableToResolve () {
+          console.log('WORKER: fetch request failed in both cache and network.', event.request.url)
+          return new Response('<h1>Service Unavailable</h1>', {
+            status: 503,
+            statusText: 'Service Unavailable',
+            headers: new Headers({
+              'Content-Type': 'text/html'
+            })
+          })
+        }
+      })
+  )
+})
